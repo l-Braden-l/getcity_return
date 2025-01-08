c93980d83cbf22b193fa3b50cec073838edc7c5d
braden <leachbr787@northwested.org>
2025-01-08 13:53:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 #Braden Leach
 #january 8 2025
-#Return City Function
+#Return City Function 
 
 
+
+#----1----#
+def get_city():
+    city = input('Please enter the name of the city you live in: ')
+    return city
+
+
+#----2----#
+def get_age():
+    age = int(input('Please enter your age: '))
+    return age
+
+
+#----3----#
+def build_output(city,age):
+    message = f'You live in {city} and you are {age} years old.'
+    return message 
+
+
+
+age = get_age()
+city = get_city()
+message = build_output(city,age)
+
+print(message)
+
+
+
+#-----really simple version-----#
 
 #----1----#
 def get_city():
